0ebc56093e4e3febb506a12358583762946a1975
Ryan Dougherty <redoughe@asu.edu>
2014-06-13 09:13:42 -0700

    Finish #1

---
+++ src/problems1_100/Problem1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problems1_100;
+
+import projecteulerinterface.ProjectEulerInterface;
+
+/**
+ *
+ * @author redoughe
+ */
+public class Problem1 implements ProjectEulerInterface {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem1().run());
+    }
+
+    @Override
+    public String run() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return Integer.toString(sum);
+    }
+}
