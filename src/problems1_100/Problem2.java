64c323616793237ad85a8efbeb381bc20cfe8af2
Ryan Dougherty <redoughe@asu.edu>
2014-06-13 13:00:42 -0700

    Problem 2 finished

---
+++ src/problems1_100/Problem2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package problems1_100;
+
+import projecteulerinterface.ProjectEulerInterface;
+
+/**
+ *
+ * @author redoughe
+ */
+public class Problem2 implements ProjectEulerInterface {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem2().run());
+    }
+    
+    @Override
+    public String run() {
+        int i1 = 1;
+        int i2 = 1;
+        int fibo = 1;
+        long sum = 0;
+        for (int i = 3; fibo <= 4000000; i++) {
+            
+            fibo = i1 + i2;
+            if (fibo % 2 == 0) {
+                sum += fibo;
+            }
+            i1 = i2;
+            i2 = fibo;
+        }
+        return String.valueOf(sum);
+    }
+    
+}
