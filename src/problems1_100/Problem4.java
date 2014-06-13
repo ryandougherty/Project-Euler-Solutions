edf3dc6d3d4945b243087b3bd846c95bb7625bc4
Ryan Dougherty <redoughe@asu.edu>
2014-06-13 13:13:38 -0700

    Problem 4 finished

---
+++ src/problems1_100/Problem4.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package problems1_100;
+
+import projecteulerinterface.ProjectEulerInterface;
+
+/**
+ *
+ * @author redoughe
+ */
+public class Problem4 implements ProjectEulerInterface {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem4().run());
+    }
+    
+    @Override
+    public String run() {
+        int min = 100;
+        int max = 999;
+        int result = 0;
+        for (int i = min; i<=max; i++) {
+            for (int j = min; j<=max; j++) {
+                if (isPalindrome(i*j)) {
+                    if (i*j > result) {
+                        result = i*j;
+                    }
+                }
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    private boolean isPalindrome(int i) {
+        String s = String.valueOf(i);
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+    
+}
