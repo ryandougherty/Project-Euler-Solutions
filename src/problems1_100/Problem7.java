085b2add79fb435078c67fc7ec93a78e81fba9f6
Ryan Dougherty <redoughe@asu.edu>
2014-06-13 13:29:31 -0700

    Problem 7 finished

---
+++ src/problems1_100/Problem7.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package problems1_100;
+
+import projecteulerinterface.ProjectEulerInterface;
+
+/**
+ *
+ * @author redoughe
+ */
+public class Problem7 implements ProjectEulerInterface {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem7().run());
+    }
+    
+    @Override
+    public String run() {
+        int count = 0;
+        for (int i = 2; ;i++) {
+            if (isPrime(i)) {
+                count++;
+                if (count == 10001) {
+                    return String.valueOf(i);
+                }
+            }
+        }
+    }
+
+    private boolean isPrime(int i) {
+        int sqrt = (int) Math.floor(Math.sqrt(i));
+        for (int j = 2; j<=sqrt; j++) {
+            if (i%j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
