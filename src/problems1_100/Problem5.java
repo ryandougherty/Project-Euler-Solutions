694d61d2e642e37ee3c11ed815c477381e222ca1
Ryan Dougherty <redoughe@asu.edu>
2014-06-13 13:19:28 -0700

    Problem 5 finished

---
+++ src/problems1_100/Problem5.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problems1_100;
+
+import java.math.BigInteger;
+import projecteulerinterface.ProjectEulerInterface;
+
+/**
+ *
+ * @author redoughe
+ */
+public class Problem5 implements ProjectEulerInterface {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem5().run());
+    }
+
+    @Override
+    public String run() {
+        BigInteger lcm = BigInteger.ONE;
+        int min = 1, max = 20;
+        for (int i = min; i <= max; i++) {
+            lcm = calculateLCM(BigInteger.valueOf(i), lcm);
+        }
+        return lcm.toString();
+    }
+
+    private static BigInteger calculateLCM(BigInteger x, BigInteger y) {
+        return x.divide(x.gcd(y)).multiply(y);
+    }
+}
